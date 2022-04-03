46b64c88b4ee0260224d24c4e0c421fd018d4bed
mdjurkovic <mdjurkovic@godaddy.com>
2022-04-03 16:09:00 +0200

    Apollo server and Graphql setup

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import graphqlServer from './graphql';
 
 const app = express();
+
+graphqlServer.applyMiddleware({
+    app,
+});
 
 export default app;
--- graphql/index.js
+++ graphql/index.js
@@ -1,41 +1,10 @@
-var graphql = require('graphql');
+import { ApolloServer } from 'apollo-server-express';
+import { env } from '../config/environment';
+import schema from "./schema";
 
-// Maps id to User object
-var fakeDatabase = {
-    'a': {
-        id: 'a',
-        name: 'alice',
-    },
-    'b': {
-        id: 'b',
-        name: 'bob',
-    },
-};
-
-// Define the User type
-var userType = new graphql.GraphQLObjectType({
-    name: 'User',
-    fields: {
-        id: { type: graphql.GraphQLString },
-        name: { type: graphql.GraphQLString },
-    }
+const apolloServer = new ApolloServer({
+    schema,
+    playground: env.development,
 });
 
-// Define the Query type
-var queryType = new graphql.GraphQLObjectType({
-    name: 'Query',
-    fields: {
-        user: {
-            type: userType,
-            // `args` describes the arguments that the `user` query accepts
-            args: {
-                id: { type: graphql.GraphQLString }
-            },
-            resolve: (_, {id}) => {
-                return fakeDatabase[id];
-            }
-        }
-    }
-});
-
-exports.UserSchema = new graphql.GraphQLSchema({query: queryType});
+export default apolloServer;
